a778ecebdc68166d6d63addebfc3862aaae8cccb
David Rearte <dengue8830@gmail.com>
2017-01-31 16:46:07 -0300

    Agregando interfaz para volley y signalr

---
+++ radiotul-sdk/src/main/java/com/amla/radiotulsdk/Constants.java
@@ -0,0 +1,9 @@
+package com.amla.radiotulsdk;
+
+/**
+ * Created by dengue8830 on 1/31/17.
+ */
+
+public class Constants {
+    public static final String SERVER_URL = "https://radiotul.com";
+}
---
+++ radiotul-sdk/src/main/java/com/amla/radiotulsdk/RadiotulSdk.java
@@ -0,0 +1,106 @@
+package com.amla.radiotulsdk;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.amla.radiotulsdk.volley.RadiotulHurlStack;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by dengue8830 on 1/31/17.
+ */
+
+public class RadiotulSdk {
+    public static final String TAG = RadiotulSdk.class.getName();
+    private Context mContext;
+    private String mToken, mPackageName, mClientId;
+    private long mCompanyId, mRadioId;
+    private RequestQueue mRequestQueue;
+    private static RadiotulSdk mInstance;
+
+    /**
+     * Main method of the sdk. Implements singleton pattern
+     * @param context
+     * @param clientId
+     * @param token
+     */
+    public void initialize(@NonNull Context context, @NonNull String clientId, @NonNull String token, long companyId, long radioId){
+        if(mInstance == null)
+            mInstance = new RadiotulSdk(context, context.getApplicationContext().getPackageName(), clientId, token, companyId, radioId);
+    }
+
+    /**
+     * The only way of get the instance of the sdk
+     * @return
+     */
+    public static RadiotulSdk getInstance(){
+        return mInstance;
+    }
+
+    /**
+     * Helper to know if the sdk is initialized
+     * @return
+     */
+    public static boolean isInitialized(){
+        return mInstance != null;
+    }
+
+    /**
+     * We prevent to instanciate the class out of it
+     */
+    private RadiotulSdk(Context context, String packageName, String clientId, String token, long companyId, long radioId){
+        mContext = context;
+        mToken = token;
+        mClientId = clientId;
+        mPackageName = packageName;
+        mCompanyId = companyId;
+        mRadioId = radioId;
+    }
+
+    /**
+     * The request queue implements singleton too
+     * @return the request queue
+     */
+    private RequestQueue getRequestQueue() {
+        if (mRequestQueue == null)
+            mRequestQueue = Volley.newRequestQueue(mContext.getApplicationContext(), new RadiotulHurlStack());
+
+        return mRequestQueue;
+    }
+
+    /**
+     * Use this method to start a request
+     * @param req the request
+     * @param <T> type of request
+     */
+    public <T> void startRequest(Request<T> req) {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+
+    public Context getContext() {
+        return mContext;
+    }
+
+    public String getToken() {
+        return mToken;
+    }
+
+    public String getPackageName() {
+        return mPackageName;
+    }
+
+    public String getClientId() {
+        return mClientId;
+    }
+
+    public long getRadioId() {
+        return mRadioId;
+    }
+
+    public long getCompanyId() {
+        return mCompanyId;
+    }
+}
